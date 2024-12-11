6eb35e9b92142671bc8483248c92b4b5309e60d6
CodeByRahul <139948879+987336@users.noreply.github.com>
2024-12-11 10:59:48 +0530

    Update BlogPage.vue

--- frontend/src/components/BlogPage.vue
+++ frontend/src/components/BlogPage.vue
@@ -41,7 +41,7 @@
           <h3>{{ blog.title }}</h3>
           <p>{{ blog.content.split(' ').slice(0, 5).join(' ') + '...' }}</p>
           <img
-            :src="blog.image ? `https://blog-backend-xka3.onrender.com${blog.image}` : 'https://images.unsplash.com/photo-1733778567699-292f5e9354d6?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'"
+            src="https://images.unsplash.com/photo-1733778567699-292f5e9354d6?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Blog Image"
             class="blog-image"
           />
