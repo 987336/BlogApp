f6920eba2430c362790a072a4663ef60d4e09fc4
CodeByRahul <139948879+987336@users.noreply.github.com>
2025-05-16 15:47:27 +0530

    Update ImageUpload.vue

--- frontend/src/components/ImageUpload.vue
+++ frontend/src/components/ImageUpload.vue
@@ -3,7 +3,7 @@
       <div class="profile-image-container" @click="triggerFileInput">
         <img
           v-if="images.length"
-          :src="`http://localhost:5000/${images[0].imagePath}`"
+          :src="${images[0].imagePath}`"
           alt="Profile Image"
           class="profile-image"
         />
